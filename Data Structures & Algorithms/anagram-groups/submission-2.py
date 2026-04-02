c867fba049504831bbe82ca0219179c9122885c6
Julian Vara <122225492+jvara34@users.noreply.github.com>
2026-04-02 16:07:05 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #create a hashmap
+        hashmap = {} 
+        #loop through each string
+        for cur in strs: 
+            #sort the current_string
+            sort_string = ''.join(sorted(cur)) # = act
+                #if sorted string is already a key in the hashmap
+            if sort_string in hashmap: 
+                    # 'in' checks the keys  
+                    # 'act' -> key 
+                    #hashmap[key].append(current_string)
+                hashmap[sort_string].append(cur)   
+                #else
+            else: 
+                hashmap[sort_string] = [cur]
+                    #hashmap[key] = [current_string]
+        #solution = []
+        solution = [] 
+        #loop through the values of hashmap
+        for value in hashmap.values(): 
+            solution.append(value) 
+            #append value to the solution
+        return solution

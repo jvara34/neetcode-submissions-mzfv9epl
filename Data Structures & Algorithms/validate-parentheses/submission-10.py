0b5b014f734a0fa567c490155ffb9efa31663724
Julian Vara <122225492+jvara34@users.noreply.github.com>
2026-04-06 11:45:44 -0700

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # create a stack 
+        stack = [] 
+        # create a map which maps the closed and the open brackets 
+        hashmap = {')' : '(', ']' : '[', '}' : '{'} 
+        # Key, close, value -> open 
+
+        # Everytime there is a closed value it needs to make sure that the stack matches with open value 
+
+        # ['()']
+
+
+        # loop through the string 
+        for char in s: 
+            # check current character is in the map 
+            if char in hashmap: # making sure that the key is hashmap 
+                # check if stack is not empty AND check if top of stack == hashmap value 
+                if stack and stack[-1] == hashmap[char]:
+                    # if correct then pop the value from the stack 
+                    stack.pop() 
+                # else 
+                else: 
+                    # reuturn false 
+                    return False
+            # else:
+            else: 
+                # stack.append(c)
+                stack.append(char)
+            
+        # return true if Stack is empty
+        if not stack: # checks if stack is empty 
+            return True
+        else: 
+            return False
+        
+        
